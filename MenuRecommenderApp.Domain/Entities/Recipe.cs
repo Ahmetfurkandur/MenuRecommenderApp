fcc7a33a15967a039c0647638873b990358ae82f
Ahmetfurkandur <fkn3388@hotmail.com>
2025-09-13 13:47:38 +0300

    Domain entities and enums added

---
+++ MenuRecommenderApp.Domain/Entities/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using MenuRecommenderApp.Domain.Enums;
+
+namespace MenuRecommenderApp.Domain.Entities
+{
+    public class Recipe
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public int DurationMin { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public List<string> Required { get; set; }
+        public List<string> Missing { get; set; }
+        public List<string> Steps { get; set; }
+        public List<string> Tips { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
